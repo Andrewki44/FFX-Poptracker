8458e8556025349291a0b9f3e52b38b4af5ebeed
Andrew K <andrewkilbourn1996@gmail.com>
2025-11-25 10:10:22 +1100

    Baaj Temple Locations

--- scripts/logic/regions.lua
+++ scripts/logic/regions.lua
@@ -1,4 +1,80 @@
 -- Region Logic
+function hasAirship()
+    return has("region:airship")
+end
+
+function hasBaaj()
+    return has("region:baajtemple")
+end
+
 function hasBesaid()
     return has("region:besaid")
+end
+
+function hasBevelle()
+    return has("region:bevelle")
+end
+
+function hasBikanel()
+    return has("region:bikanel")
+end
+
+function hasCalmLands()
+    return has("region:calmlands")
+end
+
+function hasCavern()
+    return has("region:cavernofthestolenfayth")
+end
+
+function hasDjose()
+    return has("region:djose")
+end
+
+function hasGagazet()
+    return has("region:mt.gagazet")
+end
+
+function hasGuadosalam()
+    return has("region:guadosalam")
+end
+
+function hasKilika()
+    return has("region:kilika")
+end
+
+function hasLuca()
+    return has("region:luca")
+end
+
+function hasMacalania()
+    return has("region:macalania")
+end
+
+function hasMiihen()
+    return has("region:miihenhighroad")
+end
+
+function hasMoonflow()
+    return has("region:moonflow")
+end
+
+function hasMRR()
+    return has("region:mushroomrockroad")
+end
+
+function hasOmega()
+    return has("region:omegaruins")
+end
+
+function hasSin()
+    return has("region:sin")
+end
+
+function hasThunderPlains()
+    return has("region:thunderplains")
+end
+
+function hasZanarkand()
+    return has("region:zanarkandruins")
 end
