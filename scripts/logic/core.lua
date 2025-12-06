b9d1a8085a35da90eb1a531e58347ee57a405f33
Andrewki44 <andrewkilbourn1996@gmail.com>
2025-12-06 21:17:20 +1100

    Celestial & Super Boss logic references

--- scripts/logic/core.lua
+++ scripts/logic/core.lua
@@ -2,11 +2,3 @@
 function has(item)
     return Tracker:ProviderCountForCode(item) > 0
 end
-
-function superBosses()
-    if has("superbosses") then
-        return true
-    else
-        return false
-    end
-end
--- scripts/logic/regions.lua
+++ scripts/logic/regions.lua
@@ -78,3 +78,59 @@ end
 function hasZanarkand()
     return has("region:zanarkandruins")
 end
+
+RegionDifficulty = {
+    ["Guadosalam"] = 1,
+    ["Baaj"] = 1,
+    ["Besaid"] = 2,
+    ["Kilika"] = 3,
+    ["Luca"] = 4,
+    ["Miihen"] = 5,
+    ["MushroomRock"] = 6,
+    ["Djose"] = 7,
+    ["Moonflow"] = 8,
+    ["ThunderPlains"] = 10,
+    ["Macalania"] = 11,
+    ["Bikanel"] = 12,
+    ["Airship"] = 13,
+    ["Bevelle"] = 13,
+    ["CalmLands"] = 14,
+    ["Cavern"] = 14,
+    ["Gagazet"] = 15,
+    ["Zanarkand"] = 16,
+    ["Sin"] = 17,
+    ["Omega"] = 18
+}
+-- RegionDifficulty = {
+--     [1] = {"Guadosalam", "Baaj"},
+--     [2] = {"Besaid"},
+--     [3] = {"Kilika"},
+--     [4] = {"Luca"},
+--     [5] = {"Miihen"},
+--     [6] = {"MushroomRock"},
+--     [7] = {"Djose"},
+--     [8] = {"Moonflow"},
+--     [9] = {},
+--     [10] = {"ThunderPlains"},
+--     [11] = {"Macalania"},
+--     [12] = {"Bikanel"},
+--     [13] = {"Airship", "Bevelle"},
+--     [14] = {"CalmLands", "Cavern"},
+--     [15] = {"Gagazet"},
+--     [16] = {"Zanarkand"},
+--     [17] = {"Sin"},
+--     [18] = {"Omega"}
+-- }
+CurrentMaxDifficulty = 1
+
+function CheckLogic(Region)
+    local LogicDifficulty = Tracker:ProviderCountForCode("logicdifficulty")
+
+    if (RegionDifficulty[Region] > CurrentMaxDifficulty) then
+        print(RegionDifficulty[Region])
+    end
+end
+
+-- function CheckMax()
+--     print()
+-- end
